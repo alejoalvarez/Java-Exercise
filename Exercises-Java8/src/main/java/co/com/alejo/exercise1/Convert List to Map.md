7ca47d788576edd964c17381354da18d8786f47e
Alejandro Vargas Alvarez <avalvar@bancolombia.com.co>
2021-02-13 22:07:30 -0500

    Implementation exercise1 Convert List to Map

--- Exercises-Java8/src/main/java/co/com/alejo/exercise1/Convert List to Map.md
+++ Exercises-Java8/src/main/java/co/com/alejo/exercise1/Convert List to Map.md
@@ -2,16 +2,9 @@
 # Convert List to Map
 ---
 
-This is an example that show yoy how to convert **List** of objects into a **Map**.
+This is an example that show you how to convert **List** of objects into a **Map**, and how to handle the duplicated keys.
 
 Create a list of the **PageWeb** objects, and uses **Collectors.map** to convert it into a **Map**.
 
-This is the result:
-```
-<<< Output >>>
-
-Result 1 : {1=aws.com, 2=youtube.com, 3=netflix.com, 4=gmail.com, 5=hotmail.com}
-Result 2 : {aws.com=80000, youtube.com=1, netflix.com=120000, gmail=200000, hotmail.com=90000}
-```
 
 
---
+++ Exercises-Java8/src/main/java/co/com/alejo/exercise1/ConvertListToMap2.java
@@ -0,0 +1,38 @@
+package co.com.alejo.exercise1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConvertListToMap2 {
+
+    public static void main(String[] args) {
+
+        List<PageWeb> list = new ArrayList<>();
+        list.add(new PageWeb(1, "aws.com", 80000));
+        list.add(new PageWeb(2, "youtube.com", 90000));
+        list.add(new PageWeb(3, "netflix.com", 120000));
+        list.add(new PageWeb(4, "gmail.com", 200000));
+        list.add(new PageWeb(5, "hotmail.com", 1));
+
+        list.add(new PageWeb(6, "aws.com", 100000)); //
+
+        // Print result1 withthe old value for aws
+        Map<String, Long> result1 = list.stream().collect(
+                Collectors.toMap(PageWeb::getName, PageWeb::getWebsites,
+                        (oldValue, newValue) -> oldValue
+                )
+        );
+
+        // Print result2 with the newValue for aws
+        Map<String, Long> result2 = list.stream().collect(
+                Collectors.toMap(PageWeb::getName, PageWeb::getWebsites,
+                        (oldValue, newValue) -> newValue
+                )
+        );
+
+        System.out.println("Result 1 : " + result1);
+        System.out.println("Result 2 : " + result2);
+    }
+}
