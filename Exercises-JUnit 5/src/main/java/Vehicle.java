855f2ac4352fab6fb23bff89504d55dcd34fc627
Alejandro Vargas Alvarez <avalvar@bancolombia.com.co>
2021-03-05 10:01:08 -0500

    Delete unused files

--- Exercises-JUnit 5/src/main/java/Vehicle.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.lang.reflect.Executable;
-
-public class Vehicle {
-
-    public void drive(){
-        System.out.println("Vehicle: drive");
-    }
-}
--- Exercises-JUnit 5/src/main/java/app.java
+++ Exercises-JUnit 5/src/main/java/app.java
@@ -2,17 +2,5 @@ public class app {
 
     public static void main(String[] args) {
 
-        int a = 2;
-
-        switch (a){
-            case 1:
-                System.out.println("aaa");
-            case 2:
-            case 3:
-                System.out.println("j");
-            case 4:
-                System.out.println("aaa");
-        }
-
     }
 }
